b9632017299fbfc9bda6dd538cd038e396a292be
Drull <pcaetanop@protonmail.com>
2023-02-09 22:33:03 -0300

    Adding some routes

--- index.js
+++ index.js
@@ -1,52 +1,41 @@
-const http = require("http");
-const fs = require("fs");
-const formidable = require("formidable");
+const fileUpload = require('express-fileupload');
+const express = require('express');
+const fs = require('fs')
+const app = express();
 
-const server = http.createServer((req, res) => {
-  // upload files
-  if (req.url === "/fileupload") {
-    const form = new formidable.IncomingForm();
-    form.parse(req, (err, fields, files) => {
-      if (err) {
-        res.write("Error uploading file");
-        res.end();
-      }
-      const oldPath = files.filetoupload.filepath;
-      const folderName = "uploads";
-      const newPath = `./${folderName}/${files.filetoupload.originalFilename}`;
+// Use fileUpload middleware to handle file uploads
+app.use(fileUpload());
 
-      // Check if the folder exists
-      fs.promises.access(folderName)
-        .catch(() => {
-          // If the folder doesn't exist, create it
-          return fs.promises.mkdir(folderName);
-        })
-        .then(() => {
-          // Move the file to the folder
-          fs.rename(oldPath, newPath, (err) => {
-            if (err) {
-              console.log(err);
-              res.write("Error uploading file");
-              res.end();
-            }
-            res.write("File uploaded and moved!");
-            res.end();
-          });
-        });
-    });
-  } else {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write(`
-      <form action="/fileupload" method="post" enctype="multipart/form-data">
-        <input type="file" name="filetoupload">
-        <input type="submit">
-      </form>
-    `);
-    return res.end();
-  }
+// Route for downloading a file
+app.get('/download/:fileName', (req, res) => {
+  const fileName = req.params.fileName;
+  res.download(`./files/${fileName}`);
 });
 
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+// Route for uploading a file
+app.post('/upload', (req, res) => {
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
+
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  let file = req.files.sampleFile;
+
+  // Use the mv() method to place the file in upload directory (i.e. "./files")
+  file.mv(`./files/${file.name}`, function(err) {
+    if (err) return res.status(500).send(err);
+
+    res.send('File uploaded!');
+  });
+});
+
+//list files in the files folder
+app.get('/files', (req, res) => {
+  files = fs.readdirSync("./files")
+  res.send(files)
+})
+
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000...');
 });
