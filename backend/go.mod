fd39cf8e9f87344b086f74ee60dd552302e60a9c
Gabriel Pereira Monte <gabrielpmonte@hotmail.com>
2023-02-15 19:36:27 -0300

    improve database and remove js files

--- backend/go.mod
+++ backend/go.mod
@@ -2,4 +2,4 @@ module github.com/drull1000/notetaking-app
 
 go 1.19
 
-require github.com/mattn/go-sqlite3 v1.14.16 // indirect
+require github.com/mattn/go-sqlite3 v1.14.16
--- backend/main.go
+++ backend/main.go
@@ -1,134 +1,132 @@
 package main
 
 import (
-    "database/sql"
-    "fmt"
-    "strings"
-    "log"
-    _ "github.com/mattn/go-sqlite3"
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
-//File is the struct for the file the user is going to upload.
-type File struct{
-  filename string
-  path string
-  tags []string
+// TODO: Implement md5 hash to compare files
+// File is the struct for the file the user is going to upload.
+type File struct {
+	// hash string
+	filename string
+	tags     []string
 }
 
 func main() {
-    db, err := sql.Open("sqlite3", "./database.db")
-    checkErr(err)
+	if err := os.Remove("database.db"); err != nil {
+		log.Fatal(err)
+	}
 
-    defer db.Close()
+	db, err := sql.Open("sqlite3", "./database.db")
+	checkErr(err)
+	defer db.Close()
 
-  sqlStmt := `
-  CREATE TABLE IF NOT EXISTS files (
-    uid INTEGER PRIMARY KEY AUTOINCREMENT,
-    filename VARCHAR(30) NOT NULL,
-    tags VARCHAR(64) NULL,
-    created_at TEXT DEFAULT CURRENT_TIMESTAMP
-);
-	`
+	sqlStmt := `
+	CREATE TABLE IF NOT EXISTS files (
+    	uid INTEGER PRIMARY KEY AUTOINCREMENT,
+    	filename VARCHAR(30) NOT NULL,
+    	tags VARCHAR(64) NULL,
+    	created_at TEXT DEFAULT CURRENT_TIMESTAMP
+	);`
+
 	_, err = db.Exec(sqlStmt)
 	if err != nil {
 		log.Printf("%q: %s\n", err, sqlStmt)
 		return
 	}
 
-  f := File{
-    filename: "example.txt",
-    tags:     []string{"example", "text", "file"},
-  }
+	f := File{
+		filename: "example.txt",
+		tags:     []string{"example", "text", "file"},
+	}
 
-  g := File{
-    filename: "example.txt",
-    tags:     []string{"text", "file"},
-  }
+	g := File{
+		filename: "example.txt",
+		tags:     []string{"text", "file"},
+	}
 
-  id := insert(db, f)
-  update(db, id, g)
-  delete(db, id)
-}
-    
-func update(db *sql.DB, id int64, file File) {
-    stmt, err := db.Prepare("UPDATE files SET filename=? WHERE uid=?")
-    checkErr(err)
+	h := File{
+		filename: "another_file.txt",
+		tags:     []string{"text", "file"},
+	}
 
-    res, err := stmt.Exec("astaxieupdate", id)
-    checkErr(err)
+	i := File{
+		filename: "example.txt",
+		tags:     []string{"text"},
+	}
 
-    affect, err := res.RowsAffected()
-    checkErr(err)
+	f_id := insert(db, f)
+	g_id := insert(db, g)
+	insert(db, i)
 
-    fmt.Println(affect)
+	update(db, g_id, h)
+	delete(db, f_id)
 
-    // query
-    rows, err := db.Query("SELECT * FROM files")
-    checkErr(err)
-    var uid int
-    var filename string
-    var path string
-    var tags string
-    var created string
-
-    for rows.Next() {
-        err = rows.Scan(&uid, &filename, &tags, &created)
-        checkErr(err)
-        fmt.Println(uid)
-        fmt.Println(filename)
-        fmt.Println(path)
-        fmt.Println(tags)
-        fmt.Println(created)
-    }
-
-    rows.Close() //good habit to close
-
+	getAll(db)
 }
 
-func delete(db *sql.DB, id int64){
-    stmt, err := db.Prepare("DELETE from FILES WHERE uid=?")
-    checkErr(err)
-
-    res, err := stmt.Exec(id)
-    checkErr(err)
-
-    affect, err := res.RowsAffected()
-    checkErr(err)
-
-    fmt.Println(affect)
-
-    db.Close()
-}
-
-//insert filename and tags. This is not upload.
+// insert filename and tags. This is not upload.
 func insert(db *sql.DB, file File) int64 {
-  stmt, err := db.Prepare("INSERT INTO files(filename, tags) values(?, ?)")
-  checkErr(err)
+	stmt, err := db.Prepare("INSERT INTO files(filename, tags) values(?, ?)")
+	checkErr(err)
 
-  tags := file.tags
+	tags := file.tags
+	tagsString := strings.Join(tags, ";")
 
-  res, err := stmt.Exec(file.filename, strings.Join(tags, ";"))
-  checkErr(err)
+	res, err := stmt.Exec(file.filename, tagsString)
+	checkErr(err)
 
-  id, err := res.LastInsertId()
-  checkErr(err)
+	id, err := res.LastInsertId()
+	checkErr(err)
 
-  return id
+	return id
 }
 
-func get_all(db *sql.DB, file File){
-  stmt, err := db.Prepare("SELECT * FROM files")
-  checkErr(err)
+func update(db *sql.DB, id int64, file File) {
+	stmt, err := db.Prepare("UPDATE files SET tags=? WHERE uid=?")
+	checkErr(err)
 
-  res, err := stmt.Exec()
-  checkErr(err)
+	tags := file.tags
+	tagsString := strings.Join(tags, ";")
 
-  fmt.Println(res)
+	_, err = stmt.Exec(tagsString, id)
+	checkErr(err)
 }
 
+func delete(db *sql.DB, id int64) {
+	stmt, err := db.Prepare("DELETE from FILES WHERE uid=?")
+	checkErr(err)
+
+	_, err = stmt.Exec(id)
+	checkErr(err)
+}
+
+func getAll(db *sql.DB) {
+	rows, err := db.Query("SELECT * FROM files")
+	checkErr(err)
+
+	var uid int
+	var filename string
+	var tags string
+	var created string
+
+	for rows.Next() {
+		err = rows.Scan(&uid, &filename, &tags, &created)
+		checkErr(err)
+		fmt.Println(uid, filename, tags, created)
+	}
+
+	rows.Close()
+}
 
 func checkErr(err error) {
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		log.Fatal(err)
+	}
 }
