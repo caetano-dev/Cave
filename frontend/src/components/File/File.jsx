e79c2b2b8883cb29a937c8c13fe45f1b50fb1583
Drull <pcaetanop@protonmail.com>
2023-03-19 09:11:06 -0300

    Updating quill toolbar

--- frontend/src/components/File/File.jsx
+++ frontend/src/components/File/File.jsx
@@ -1,10 +1,9 @@
-import React, {useEffect, useState} from 'react';
-import './File.css';
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
+import React, { useEffect, useState } from "react";
+import "./File.css";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
-function File({ toggle, filename, id, content, setContent}) {
-
+function File({ toggle, filename, id, content, setContent }) {
   const editContent = async (id) => {
     try {
       const response = await fetch("http://localhost:3000/fileEditContent", {
@@ -17,8 +16,8 @@ function File({ toggle, filename, id, content, setContent}) {
           content: content,
         }),
       });
-      if (!response.ok){
-        throw new Error("Failed to edit file content")
+      if (!response.ok) {
+        throw new Error("Failed to edit file content");
       }
     } catch (error) {
       console.error(error);
@@ -27,27 +26,49 @@ function File({ toggle, filename, id, content, setContent}) {
   };
 
   useEffect(() => {
-    function handleSaveShortcutKeyDown(event){
-      if(event.ctrlKey && event.key === 's'){
+    function handleSaveShortcutKeyDown(event) {
+      if (event.ctrlKey && event.key === "s") {
         event.preventDefault();
-        editContent(id)
-        setContent(content)
-        console.log("content updated: "+content)
+        editContent(id);
+        setContent(content);
+        console.log("content updated: " + content);
       }
     }
-    document.addEventListener("keydown", handleSaveShortcutKeyDown)
-    return ()=>{
-      document.removeEventListener('keydown', handleSaveShortcutKeyDown)
-    }
-  },[content]);
+    document.addEventListener("keydown", handleSaveShortcutKeyDown);
+    return () => {
+      document.removeEventListener("keydown", handleSaveShortcutKeyDown);
+    };
+  }, [content]);
+
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, 3, 4, 5, 6, false] }, { font: [] }],
+      [{ size: [] }],
+      [{ color: [] }, { background: [] }],
+      ["bold", "italic", "underline", "strike", "blockquote", "code-block"],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
+        { direction: "rtl" },
+      ],
+      ["link", "image", "video"],
+      ["clean"],
+    ],
+  };
 
   return (
     <main className={toggle}>
-      <h1 className='title'>{filename}</h1>
-      <ReactQuill theme="snow" value={content} onChange={setContent} />
+      <h1 className="title">{filename}</h1>
+      <ReactQuill
+        modules={modules}
+        theme="snow"
+        value={content}
+        onChange={setContent}
+      />
     </main>
   );
 }
 
 export default File;
-
