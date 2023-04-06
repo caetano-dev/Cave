e3d4637c8cd49a71cc281849aef3a51c9ff28d3a
Drull <pcaetanop@protonmail.com>
2023-04-06 17:09:28 -0300

    Adding guillemets

--- frontend/src/components/ToggleButton/ToggleButton.jsx
+++ frontend/src/components/ToggleButton/ToggleButton.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-function ToggleButton({toggle, toggleState}) {
+function ToggleButton({ toggle, toggleState }) {
   return (
     <button
       className={"sideBarButton " + toggle}
       title="Toggle sidebar"
       onClick={toggleState}
     >
-      ☰
+      {toggle === "open" ? "«" : "»"}
     </button>
   );
 }
